849bddb11c0a783b9ded628bab6bd4db468d276c
Botond0426 <szilagyibotond07@gmail.com>
2023-12-18 21:31:28 +0200

    Initial commit

---
+++ src/main/java/pages/BasePage.java
@@ -0,0 +1,34 @@
+package pages;
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+
+public class BasePage {
+    public static WebDriver driver;
+    private String BASE_URL =
+            "https://www.lambdatest.com/selenium-playground/";
+    public BasePage() {
+// Default constructor
+    }
+    public BasePage(WebDriver driver) {
+        BasePage.driver = driver;
+    }
+    @BeforeMethod
+    public void setUp() {
+        WebDriverManager.firefoxdriver().setup();
+        FirefoxOptions options = new FirefoxOptions();
+        options.addArguments("--headless");
+        driver = new FirefoxDriver(options);
+        driver.manage().window().maximize();
+        driver.get(BASE_URL);
+    }
+    @AfterMethod
+    public void tearDown() {
+        if (driver != null) {
+            driver.quit();
+        }
+    }
+}
